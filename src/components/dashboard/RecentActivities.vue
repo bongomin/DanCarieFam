c775997f4d376c1f33c7b9cee6c4b0e970cac1c3
Bongomin Daniel <bongomindaniel@gmail.com>
2024-10-26 07:09:55 +0300

    Component: RecentActivities

---
+++ src/components/dashboard/RecentActivities.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-12 col-lg-4 gt-sm">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Recent Activities</div>
+        <q-list>
+          <q-item v-for="activity in activities" :key="activity.id">
+            <q-item-section avatar>
+              <q-icon :name="activity.icon" color="primary" size="sm" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label class="text-subtitle2">{{
+                activity.description
+              }}</q-item-label>
+              <q-item-label caption>{{ activity.timestamp }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  activities: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
