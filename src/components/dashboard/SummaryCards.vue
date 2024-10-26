1b7eda73b2f946ac4bf2f700a9dd6828e251b51a
Bongomin Daniel <bongomindaniel@gmail.com>
2024-10-26 07:10:08 +0300

    Component: SummaryCards

---
+++ src/components/dashboard/SummaryCards.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-for="stat in stats" :key="stat.title" class="col-6 col-sm-6 col-md-3">
+    <q-card class="dashboard-card">
+      <q-card-section>
+        <div class="text-subtitle2 text-md-h6">{{ stat.title }}</div>
+        <div class="text-h6 text-md-h4 q-mt-sm q-mt-md-md text-weight-bold">
+          {{ stat.value }}
+        </div>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  stats: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.dashboard-card {
+  height: 100%;
+}
+</style>
